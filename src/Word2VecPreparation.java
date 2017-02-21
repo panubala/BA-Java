f3f04d59861b9df5c0351f93c16db26b79a1cb2b
panubala <panuyabalasuntharam@public-docking-cx-1115.ethz.ch>
2017-02-21 14:56:13 +0100

    This doesn't work

--- src/Word2VecPreparation.java
+++ src/Word2VecPreparation.java
@@ -1,17 +1,28 @@
+import java.awt.List;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class Word2VecPreparation {
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		UtilsPMC utils = new UtilsPMC();
+		File[] listOfFiles = new File[]{};
+		
+		File folder;
+		
+		for (int i = 0; i < 10; i++) {
+			folder = new File("/Users/panuyabalasuntharam/Documents/BA/pmc-00/0"+i);
+			listOfFiles = Stream.concat(Arrays.stream(listOfFiles), Arrays.stream(folder.listFiles())).toArray(File[]::new);
+		}
 		
 		
-		File folder = new File("/Users/panuyabalasuntharam/Documents/BA/pmc/pmc-00/00");
-		File[] listOfFiles = folder.listFiles();
+		
 		
 		String content = "";
 		for (int i = 0; i < listOfFiles.length; i++) {
@@ -20,7 +31,7 @@ public class Word2VecPreparation {
 		
 		System.out.println(content);
 		
-		Writer output = new BufferedWriter(new FileWriter("/Users/panuyabalasuntharam/Documents/BA/PMCcontent.txt"));
+		Writer output = new BufferedWriter(new FileWriter("/Users/panuyabalasuntharam/Documents/BA/PMC.txt"));
 		output.append(content);
 		output.close();
 	}
